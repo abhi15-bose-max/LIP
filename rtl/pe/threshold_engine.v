585fa12814de42cbc7e296faa5f78ce4d289d098
abhi15-bose-max <abhinavbasu156@gmail.com>
2026-06-20 22:23:22 +0530

    Update threshold_engine.v

--- rtl/pe/threshold_engine.v
+++ rtl/pe/threshold_engine.v
@@ -1,1 +1,21 @@
+module threshold_engine (
 
+    input [15:0] min_val,
+    input [15:0] max_val,
+
+    input [15:0] threshold,
+
+    output uniformity,
+    output roughness
+
+);
+
+wire [15:0] range;
+
+assign range = max_val - min_val;
+
+assign uniformity = (range < threshold);
+
+assign roughness = (range > threshold);
+
+endmodule
